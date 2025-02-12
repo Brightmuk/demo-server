f1d34b24db7bd1d39d107bdabb76366a03d21efd
Brimuk <be.rightmuk@gmail.com>
2025-02-12 09:49:12 +0800

    web Sockets

--- index.js
+++ index.js
@@ -1,38 +1,43 @@
-const WebSocket = require("ws");
+const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 3000 });
+const server = new WebSocket.Server({ port: 3000 });
+
 let clients = {};
 
-wss.on("connection", (ws) => {
-    console.log('New client connected...');
-    ws.on("message", (message) => {
-        const data = JSON.parse(message);
+server.on('connection', (socket) => {
+    console.log('New client connected');
 
+    socket.on('message', (message) => {
+        const data = JSON.parse(message);
+        console.log(JSON.stringify(data, null, 2));
         switch (data.type) {
-            case "join":
-                clients[data.clientId] = ws;
-                console.log(`Client ${data.clientId} joined`);
+            case 'join':
+                clients[data.userId] = socket;
+                console.log(`User ${data.userId} joined`);
                 break;
-            case "offer":
-            case "answer":
-            case "candidate":
-                let target = clients[data.targetId];
-                if (target) {
-                    target.send(JSON.stringify(data));
+
+            case 'offer':
+            case 'answer':
+            case 'candidate':
+                if (clients[data.target]) {
+                    clients[data.target].send(JSON.stringify(data));
                 }
+                break;
+
+            case 'leave':
+                delete clients[data.userId];
                 break;
         }
     });
 
-    ws.on("close", () => {
-        console.log("Service closed...");
-        for (let clientId in clients) {
-            if (clients[clientId] === ws) {
-                delete clients[clientId];
+    socket.on('close', () => {
+        for (let userId in clients) {
+            if (clients[userId] === socket) {
+                delete clients[userId];
                 break;
             }
         }
     });
-});
+}); 
 
-console.log("WebSocket server running on ws://localhost:3000");
+console.log("WebSocket signaling server running on ws://localhost:3000");
